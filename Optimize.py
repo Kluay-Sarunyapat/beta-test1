33f17dd7a38a9d9bdb62b848398a20f2e776ed21
Kluay-Sarunyapat <khankluay88@hotmail.com>
2025-06-10 10:52:16 +0700

    Update Optimize.py

--- Optimize.py
+++ Optimize.py
@@ -415,6 +415,7 @@ if st.session_state.page == "Simulation Budget":
 # Google Sheets CSV direct link
 sheet_url_raw = "https://docs.google.com/spreadsheets/d/1jMo9lFTxif0uwAgwJeyn60_E2jM9n5Ku/gviz/tq?tqx=out:csv"
 sheet_url_off = "https://docs.google.com/spreadsheets/d/1Fst4_Ac4SwmY4WQ1S_rzXSgmrxDb3jvp/gviz/tq?tqx=out:csv"
+sheet_url_full = "https://docs.google.com/spreadsheets/d/1f7x4teD3iBeFfhmpObHqcj8wl_DkipLwa_JxAO5sYp8/gviz/tq?tqx=out:csv"
 
 @st.cache_data
 def load_google_sheets(url):
@@ -427,9 +428,9 @@ if st.button('Refresh Data'):
 # Load the data
 df = load_google_sheets(sheet_url_raw)
 df_coff = load_google_sheets(sheet_url_off)
-
+df_full = load_google_sheets(sheet_url_full)
 # # Display the data
-# st.write(df)
+st.write(df_full)
 
 
 # # ---------- PAGE 2: Influencer Performance ----------

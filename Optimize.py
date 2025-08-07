5378aa26ca9cdbfb82ce2aaa6bd7393a0ea00feb
Kluay-Sarunyapat <khankluay88@hotmail.com>
2025-08-07 21:22:23 +0700

    Update Optimize.py

--- Optimize.py
+++ Optimize.py
@@ -207,9 +207,6 @@ for (name, icon), col in zip(buttons, cols):
             if st.button(f"{icon} {name}", key=name):
                 change_page(name)
 
-# Demo content to show which page is active (can be removed)
-st.write(f"Current Page: {st.session_state.page}")
-
 
 # ---------- FUNCTION: Load Weights from Google Sheet CSV ----------
 @st.cache_data
